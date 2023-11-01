d37cf45ad3f84e42c868d9d156ce32ff448a811b
leesooyeon21 <147388033+leesooyeon21@users.noreply.github.com>
2023-11-01 19:02:27 +0900

    전처리 전라북도

--- 전처리 및 파이토치 코드.py
+++ 전처리 및 파이토치 코드.py
@@ -1,4 +1,4 @@
-#정석님 코드를 수정했습니다. 아나콘다로 파이토치 실행했습니다.
+# vscode로 실행(전라북도 한정)
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -8,69 +8,81 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+
 from datetime import datetime
 
-print(datetime.now())
+def main():
+    print(datetime.now())
 
-chunk_size = 500000
-file_path = r"/Users/kimjehyeon/Desktop/2023_3_2/캡스톤 - Weather Seller/롯데카드데이터.csv"
-chunks = pd.read_csv(file_path, encoding='utf-8', chunksize=chunk_size)
-imputer = SimpleImputer(strategy="median")
-scaler = StandardScaler()
+    # 데이터 불러오기
+    filtered_data_list = []
+    chunk_iter = pd.read_csv(r"C:\Users\mypc\Desktop\롯데카드_소비_데이터.csv", encoding='CP949', chunksize=1000000)
 
-# 처음 작성을 위한 플래그 설정
-first_write = True
-output_file = "preprocessed_data.csv"
+    for chunk in chunk_iter:
+        filtered_chunk = chunk[chunk['ana_mgpo_nm'] == '전라북도']
+        filtered_data_list.append(filtered_chunk)
 
-for chunk in chunks:
-    chunk["sl_am"] = imputer.fit_transform(chunk[["sl_am"]])
-    chunk["sl_ct"] = imputer.fit_transform(chunk[["sl_ct"]])
-    z_scores = np.abs((chunk["sl_am"] - chunk["sl_am"].mean()) / chunk["sl_am"].std())
-    chunk = chunk[(z_scores < 3)]
-    chunk[["sl_am", "sl_ct"]] = scaler.fit_transform(chunk[["sl_am", "sl_ct"]])
+    filtered_data = pd.concat(filtered_data_list, axis=0)
     
-    # 각 청크를 처리한 후 바로 파일에 저장
-    if first_write:
-        chunk.to_csv(output_file, index=False)
-        first_write = False
-    else:
-        chunk.to_csv(output_file, mode='a', header=False, index=False)
+    # 결측치 처리
+    imputer = SimpleImputer(strategy="median")
+    filtered_data["sl_am"] = imputer.fit_transform(filtered_data[["sl_am"]])
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(6 * 13 * 13, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+    # 이상치 처리
+    z_scores = np.abs((filtered_data["sl_am"] - filtered_data["sl_am"].mean()) / filtered_data["sl_am"].std())
+    filtered_data = filtered_data[(z_scores < 3)]
+
+    # 정규화
+    scaler = StandardScaler()
+    filtered_data[["sl_am", "sl_ct"]] = scaler.fit_transform(filtered_data[["sl_am", "sl_ct"]])
+
+    # 데이터 분석 및 기술 개발 부분은 문제에 따라 추가적인 코드를 작성해야 합니다.
+
+    # 전처리된 데이터를 파일로 저장 (옵션)
+    filtered_data.to_csv("preprocessed_filtered_data.csv", index=False)
+
+    class Net(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.fc1 = nn.Linear(6 * 14 * 14, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool(torch.relu(self.conv1(x)))
+            x = x.view(-1, 6 * 14 * 14)
+            x = torch.relu(self.fc1(x))
+            x = torch.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+    net = Net()
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # 데이터셋을 두 번 반복합니다.
+        running_loss = 0.0
+        for i, data_batch in enumerate(trainloader, 0):
+            inputs, labels = data_batch
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
     
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = x.view(-1, 6 * 13 * 13)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    print('Finished Training')
+    print(datetime.now())
+    print(filtered_data.head())
+    return filtered_data
 
-net = Net()
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-for epoch in range(2):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
-
-print('Finished Training')
-print(datetime.now())
+if __name__ == '__main__':
+    main()
